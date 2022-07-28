0a950bd7ab3569618f21934926ad75a03c050966
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-28 14:21:53 -0600

    Fix indentation

--- src/Components/ArticlesContainer/ArticlesContainer.js
+++ src/Components/ArticlesContainer/ArticlesContainer.js
@@ -8,16 +8,18 @@ const ArticlesContainer = ({ displayedArticles, setCurrentArticle }) => {
     if(element.short_url) {
       return (
         <Article
-        title={element.title}
-        byline={element.byline}
-        key={element.short_url}
-        setCurrentArticle={setCurrentArticle}
-        shorturl={element.short_url}
-        displayedArticles={displayedArticles}
+          title={element.title}
+          byline={element.byline}
+          key={element.short_url}
+          setCurrentArticle={setCurrentArticle}
+          shorturl={element.short_url}
+          displayedArticles={displayedArticles}
         />
       )
     }
   })
+
+  //forEach? How does React handle elements of an array that are undefined or null?
 
   return(
     <section className='all-articles'>
