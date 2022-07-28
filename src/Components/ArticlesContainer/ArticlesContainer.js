3126715ba5e275b375edf233a93732552f6c5fce
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-28 07:10:27 -0600

    Add a key to each article to remove missing key error

--- src/Components/ArticlesContainer/ArticlesContainer.js
+++ src/Components/ArticlesContainer/ArticlesContainer.js
@@ -5,12 +5,15 @@ import './ArticlesContainer.css'
 const ArticlesContainer = ({ displayedArticles }) => {
 
   const allArticles = displayedArticles.map(element => {
-    return (
-      <Article
+    if(element.short_url) {
+      return (
+        <Article
         title={element.title}
         byline={element.byline}
-      />
-    )
+        key={element.short_url}
+        />
+      )
+    }
   })
 
   return(
