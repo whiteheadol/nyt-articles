1fe1e6d52f5123d74129ed0fa0244b027fe4800a
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-28 09:30:12 -0600

    Add a way to find the article clicked on and save as the current article

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Article.css'
 
-const Article = ({ title, byline }) => {
+const Article = ({ title, byline, setCurrentArticle, shorturl, displayedArticles }) => {
+
+  const handleCurrentArticle = (e) => {
+    let article = displayedArticles.find(element => {
+      return element.short_url === shorturl;
+    })
+    setCurrentArticle(article)
+  };
 
   return(
-    <div className='article-div'>
+    <div className='article-div' onClick={handleCurrentArticle} id={shorturl}>
       <p className='article-title'>{title}</p>
       <p className='article-byline'>{byline}</p>
     </div>
