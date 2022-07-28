578eabd6bdf3647e86611e93232acc6d6f585472
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-28 06:24:37 -0600

    Change structure of app for better styling

--- src/App.js
+++ src/App.js
@@ -15,29 +15,32 @@ function App() {
     fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=GuWKVIUUyA3DlfmPdjbouV6EFbkXQbVv")
       .then(response => response.json())
       .then(data => {
+        console.log(data.results)
         setDisplayedArticles(data.results)
         }
       )
-      .then(info => console.log('d', displayedArticles))
+      // .then(info => console.log('d', displayedArticles))
   }, [])
 
   return (
-    <div className="App">
-      <NavBar />
-      <ArticlesContainer
-        displayedArticles={displayedArticles}
-      />
-      <Article />
-      <ArticleDetails />
-      <FoF />
+    <div className='App'>
+      <div className='nav-and-details'>
+        <NavBar />
+        <ArticleDetails />
+      </div>
+        <ArticlesContainer
+          displayedArticles={displayedArticles}
+        />
     </div>
   );
 }
 
 export default App;
+// <FoF />
 
 // Pass displayed articles down to correct components to display
 // Set up a dropdown menu (Build out navBar structure)
 // Set up the most basic css for page structure
 // Dropdown menu tied to a function that will fetch and update displayedArticles accordingly
 // Worry about 404 page once funtionality is there
+// Add basic styling to each article (border?) to see if it's working correctly
