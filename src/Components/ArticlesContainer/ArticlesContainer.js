57bc78ac86384b23b3257795a29d18a9482a0288
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-28 06:25:30 -0600

    Pass information needed to each individual article

--- src/Components/ArticlesContainer/ArticlesContainer.js
+++ src/Components/ArticlesContainer/ArticlesContainer.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Article from '../Article/Article';
+import './ArticlesContainer.css'
 
 const ArticlesContainer = ({ displayedArticles }) => {
 
   const allArticles = displayedArticles.map(element => {
     return (
       <Article
+        title={element.title}
+        byline={element.byline}
       />
     )
   })
 
   return(
-    <div>
-      <p>{allArticles}</p>
+    <div className='all-articles'>
+      {allArticles}
     </div>
   )
 }
