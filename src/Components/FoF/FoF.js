8cb867da9461ef5c4ef7e6e273918437bf8549c8
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-28 20:59:18 -0600

    Add a 404 message

--- src/Components/FoF/FoF.js
+++ src/Components/FoF/FoF.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './FoF.css'
+
 
 const FoF = () => {
 
   return(
     <div>
-      <p>FOF page!</p>
+      <p className='fof-message'>Hey! You're trying to visit a page that doesn't exist. <Link to={'/'}>Go back home!</Link></p>
     </div>
   )
 }
