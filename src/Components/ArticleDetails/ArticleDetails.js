8bda406e0a6c2e168e7d813f2b44a024623e18c4
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-07-28 18:29:55 -0600

    Add methods to add an article to the saved array and render button correctly

--- src/Components/ArticleDetails/ArticleDetails.js
+++ src/Components/ArticleDetails/ArticleDetails.js
@@ -2,7 +2,22 @@ import React from 'react';
 import './ArticleDetails.css';
 import {useEffect} from 'react';
 
-const ArticleDetails = ({ currentArticle }) => {
+const ArticleDetails = ({ currentArticle, savedArticles, setSavedArticles }) => {
+
+  const checkIfSaved = (currentArticle, savedArticles) => {
+    let value = false;
+    savedArticles.forEach(element => {
+      if(element.title === currentArticle.title) {
+        value = true;
+      }
+    })
+    return value;
+  }
+
+  const addArticle = () => {
+    setSavedArticles(savedArticles => [...savedArticles, currentArticle])
+  }
+
 
   return(
     <section className='article-details'>
@@ -10,6 +25,7 @@ const ArticleDetails = ({ currentArticle }) => {
       <p className='details details-byline'>{currentArticle.byline}</p>
       <p className='details details-abstract'>{currentArticle.abstract}</p>
       {currentArticle.url && <a href={currentArticle.url} className='details-href'><p className='details-link'>Full Article</p></a>}
+      {(!checkIfSaved(currentArticle, savedArticles) && currentArticle.url)&& <button className='save-btn' onClick={addArticle}><strong>Save for Later</strong></button>}
     </section>
   )
 }
